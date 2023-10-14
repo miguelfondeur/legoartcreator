d39e1abe05e89c114fc5fa4eaff8385163113ab7
Miguel Fondeur <miguelfondeur@Miguels-MBP.lan>
2023-10-14 09:30:55 -0400

    update footer and add privacy policy

--- src/js/modules/Login/login.js
+++ src/js/modules/Login/login.js
@@ -10,25 +10,25 @@ class LoginForm extends HTMLElement {
                         <div>
                             <label for="loginEmail" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
                             <div class="mt-2">
-                                <input id="loginEmail" name="loginEmail" type="email" autocomplete="loginEmail" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#016db7] sm:text-sm sm:leading-6">
+                                <input id="loginEmail" name="loginEmail" type="email" autocomplete="loginEmail" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring- sm:text-sm sm:leading-6">
                             </div>
                         </div>
                         <div>
                             <div class="flex items-center justify-between">
                                 <label for="loginPassword" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
                                 <div class="text-sm">
-                                    <a href="#" class="font-semibold text-[#016db7] hover:text-indigo-500">Forgot password?</a>
+                                    <a href="#" class="font-semibold text- hover:text-indigo-500">Forgot password?</a>
                                 </div>
                             </div>
                             <div class="mt-2">
-                                <input id="loginPassword" name="loginPassword" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#016db7] sm:text-sm sm:leading-6">
+                                <input id="loginPassword" name="loginPassword" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring- sm:text-sm sm:leading-6">
                             </div>
                         </div>
-                        <button type="submit" class="flex w-full justify-center rounded-md bg-[#016db7] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#016db7]">Sign in</button>
+                        <button type="submit" class="flex w-full justify-center rounded-md bg- px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-">Sign in</button>
                     </form>
                     <p class="mt-10 text-center text-sm text-gray-500">
                         Not a member?
-                        <a href="/signup.html" class="font-semibold leading-6 text-[#016db7] hover:text-indigo-500">Sign up Now</a>
+                        <a href="/signup.html" class="font-semibold leading-6 text- hover:text-indigo-500">Sign up Now</a>
                     </p>
                 </div>`;
     }
